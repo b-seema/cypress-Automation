d1977bf6d0a1d62c1bc3f13373cb261e1d90fad3
b-seema <seemabhardwaj916@gmail.com>
2024-02-28 18:59:09 +0530

    Updated testscript

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,7 +3,7 @@ describe ('Official website' , function(){
 it ('search the gmail',function()
 {
 cy.visit('https://www.google.com/');
-cy.get ('#APjFqb') .type('Gmail');
+cy.get ('#APjFqb') .type('Gmail{enter}');
 
 
 }
