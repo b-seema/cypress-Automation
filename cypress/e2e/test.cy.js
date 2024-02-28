9e00607a405b15065d31840efcc07525a35e84a5
b-seema <seemabhardwaj916@gmail.com>
2024-02-28 19:16:26 +0530

    update in process. . .

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,6 +4,9 @@ it ('search the gmail',function()
 {
 cy.visit('https://www.google.com/');
 cy.get ('#APjFqb') .type('Gmail{enter}');
+// cy.get ('.dmenKe > .cIkxbf > .usJj9c > :nth-child(1) > .VttTV > :nth-child(1) > h3 > .l{enter}');
+
+// cy.contains('Sign in to Gmail').click()
 
 
 }
