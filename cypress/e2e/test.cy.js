ddd7f441745fd6b46231c2a1f8b84937d9b250ed
b-seema <seemabhardwaj916@gmail.com>
2024-02-29 18:57:41 +0530

    Swag lab website

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,11 +3,8 @@ describe ('Official website' , function(){
 it ('search the gmail',function()
 {
 cy.visit('https://www.google.com/');
-cy.get ('#APjFqb') .type('Gmail{enter}');
-// cy.get ('.dmenKe > .cIkxbf > .usJj9c > :nth-child(1) > .VttTV > :nth-child(1) > h3 > .l{enter}');
-
-// cy.contains('Sign in to Gmail').click()
-
+cy.get ('#APjFqb') .type('Swag Labs{enter}');
+cy.get('.VuuXrf') .eq(2).click();
 
 }
 )
