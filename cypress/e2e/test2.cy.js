86230f060244b5521821c22af0d387e0afd61da6
b-seema <seemabhardwaj916@gmail.com>
2024-03-02 00:51:48 +0530

    Updated test

---
+++ cypress/e2e/test2.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>"
+
+describe('Testing of Truvideo WebApp' , function (){
+it ('Open the Truvideo WebApp' , function (){
+
+
+
+cy.visit ('https://rc.truvideo.com/');
+cy.wait (2000);
+//cy.get(':nth-child(4) > .input-prepend > .input-large').type('seema5exceptions@gmail.com'); 
+//cy.get(':nth-child(5) > .input-prepend > .input-large').type('seema5exceptions@gmail.com'); 
+//cy.get('.modal-body') .find ('.input-large') .eq (2) .should ('contains', Email/Username)
+//cy.get('.modal-body') .find ('.input-large') .invoke('attr', 'placeholder').should('eq', 'Email/Username') .type ('seema')
+//cy.get (['autofocus="autofocus"']) .type ('seema')
+//cy.xpath ('//input[@name="j_username"]')
+//cy.get('input[placeholder="Email/Username"]') .type ('seema5exceptions@gmail.com')
+//cy.get ('.input-large') .eq(0).type('seema')
+cy.get ('input[autofocus="autofocus"]').first() .type('seems');
+})
+
+
+})
