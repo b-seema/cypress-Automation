5e0589d2c1aa346fe1c58485d30022a4f6f93977
b-seema <seemabhardwaj916@gmail.com>
2024-03-05 19:57:03 +0530

    Updated test

--- cypress/e2e/test2.cy.js
+++ cypress/e2e/test2.cy.js
@@ -29,19 +29,27 @@
        // });
    // });
 describe('testing of checkboxes',function(){
+
+  beforeEach(() => {
+    cy.visit('https://www.ironspider.ca/forms/checkradio.htm')
+  })
+  
+ 
+ 
 it ('select one checkbox', function(){
-cy.visit('https://www.ironspider.ca/forms/checkradio.htm');
+//cy.visit('https://www.ironspider.ca/forms/checkradio.htm');
 cy.get('form').eq(0).scrollIntoView();
 cy.get('input[value="red"]').check().should('be.checked');
 cy.get('input[value="yellow"').check().should('be.checked');
 cy.wait(3000);
 cy.get('input[type="checkbox"]').uncheck();
-
-
-
 })
 
-
+it ('Select all the check boxes at once',function(){
+//cy.visit('https://www.ironspider.ca/forms/checkradio.htm');
+cy.get('input[type="checkbox"]').check(['red','blue','purple']).should('be.checked')
+.and('have.value','red','blue','purple');
+})
 
 
 
