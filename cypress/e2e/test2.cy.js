3184ca645c19b82bb1692a36990abbce16098b8d
b-seema <seemabhardwaj916@gmail.com>
2024-03-05 18:38:40 +0530

    Updated test

--- cypress/e2e/test2.cy.js
+++ cypress/e2e/test2.cy.js
@@ -1,37 +1,46 @@
 /// <reference types="cypress"/>"
 
-describe('Testing of Truvideo WebApp' , function (){
-it ('Open the Truvideo WebApp' , function (){
+//describe('Testing Truvideo WebApp', function () {
+    //it('Open the Truvideo WebApp', function () {
+      //let errorMessageText;
+  
+      //cy.visit('https://rc.truvideo.com/');
+      //cy.wait(2000);
+  
+      //cy.get('input[placeholder="Email/Username"]').first().type('seema5exceptions@gmail.com');
+      //cy.get('input[placeholder="Password"]').type('Seema@123');
+  
+      //cy.get('.btn-primary').click();
+  
+      // Fetch the text of the error message
+     // cy.get('.alert-error')
+       // .should('be.visible')
+        //.//invoke('text')
+        //.then((text) => {
+         // errorMessageText = text;
+          // Log the error message to the Cypress Test Runner
+          //cy.log(`Error Message: ${errorMessageText}`);
+          
+          // Further assertions based on the fetched text
+          //expect(errorMessageText).to.contain('Log in Failed. The username/password you entered is incorrect');
+        
+        
+        
+       // });
+   // });
+describe('testing of checkboxes',function(){
+it ('select one checkbox', function(){
+cy.visit('https://www.ironspider.ca/forms/checkradio.htm');
+cy.get('form').eq(0).scrollIntoView();
 
-
-
-cy.visit ('https://rc.truvideo.com/');
-cy.wait (2000);
-//cy.get(':nth-child(4) > .input-prepend > .input-large').type('seema5exceptions@gmail.com'); 
-//cy.get(':nth-child(5) > .input-prepend > .input-large').type('seema5exceptions@gmail.com'); 
-//cy.get('.modal-body') .find ('.input-large') .eq (2) .should ('contains', Email/Username)
-//cy.get('.modal-body') .find ('.input-large') .invoke('attr', 'placeholder').should('eq', 'Email/Username') .type ('seema')
-//cy.get (['autofocus="autofocus"']) .type ('seema')
-//cy.xpath ('//input[@name="j_username"]')
-//cy.get('input[placeholder="Email/Username"]') .type ('seema5exceptions@gmail.com')
-//cy.get ('.input-large') .eq(0).type('seema')
-cy.get ('input[placeholder="Email/Username"]').first() .type('seema5exceptions@gmail.com');
-cy.get ('input[placeholder="Password"]') .type ('Seema@123') ;
-//cy.get ('input.btn btn-primary btn-block btn-large') .eq(1).click()
-//cy.get('.btn-primary').click();
-// cy.get ('.btn').eq(4) 
-//cy.get ('input[value="Log In"]').click();
-//cy.go('back');
-//cy.get('#register-user').should('exist');
-cy.get('#register-user').should('exist').click()
-cy.go('back');
-cy.get('#forgot-password').should('exist').click();
-cy.go('back')
 
 
 
 
 })
+
+
+
 
 
 })
